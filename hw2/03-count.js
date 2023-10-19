8c8dfba10d091430e402dfc86c422fa1b2e5ee06
David <davvan@pdx.edu>
2023-10-18 18:47:30 -0700

    finished with hw2

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,23 @@
 // Add your code here
 
-input.addEventListener('keydown', handleKeyDown);
+const text = document.querySelector("#content");
+let textString = text.textContent;
+
+addEventListener("keydown", handleKeyDown);
+
+function handleKeyDown(e) {
+  let user_input;
+  if (e.key !== "Backspace") {
+    user_input = document.querySelector("input").value + e.key;
+  } else {
+    user_input = document.querySelector("input").value;
+  }
+  console.log(user_input);
+  user_input = user_input.trim();
+  let textString = text.textContent;
+  textString = textString.replace(
+    new RegExp(user_input, "gi"),
+    (match) => `<span class="bg-warning">${match}</span>`
+  );
+  text.innerHTML = textString;
+}
--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,37 @@
 // Add your code here
+let nIntervId;
+addEventListener("load", handleLoad());
+
+function handleLoad() {
+  const btnElem = document.querySelector("button");
+  btnElem.addEventListener("click", start);
+}
+
+function start() {
+  const btnElem = document.querySelector("button");
+  const user_input = document.querySelector("select").value;
+  if (btnElem.classList.contains("start")) {
+    btnElem.classList.remove("start");
+    btnElem.classList.remove("btn-danger");
+    btnElem.classList.add("btn-primary");
+    btnElem.innerHTML = "Start";
+    clearInterval(nIntervId);
+  } else {
+    btnElem.classList.add("start");
+    btnElem.classList.remove("btn-primary");
+    btnElem.classList.add("btn-danger");
+    btnElem.innerHTML = "Stop";
+
+    nIntervId = setInterval(myTimer, user_input * 1000);
+  }
+}
+
+function randomColor() {
+  //source: https://css-tricks.com/snippets/javascript/random-hex-color/
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+
+function myTimer() {
+  const bodyElem = document.querySelector("body");
+  bodyElem.style.backgroundColor = `${randomColor()}`;
+}
