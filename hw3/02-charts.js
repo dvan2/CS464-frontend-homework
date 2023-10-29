568e246f916a981cd906099ee69b7bc18137fc13
David <davvan@pdx.edu>
2023-10-29 13:52:30 -0700

    finished 02charts

--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -28,6 +28,98 @@ const borderColors = [
 
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
+const house = [];
+
+function getThronesData(url) {
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => {
+      throw new Error(`Cannot fetch data ${error.message}`);
+    });
+}
+
+function main() {
+  getThronesData(url).then((data) => {
+    console.log(data);
+    data.forEach((character) => groupCharacters(character));
+    fixAbnormalData();
+    groupSingleCharacters();
+    console.log(house);
+
+    renderChart();
+  });
+}
+
+function groupCharacters(character) {
+  const index = house.findIndex((element) => element.name === character.family);
+  if (index !== -1) {
+    house[index].count++;
+    house[index].characterArr.push(character);
+  } else {
+    house.push({
+      name: character.family,
+      count: 1,
+      characterArr: [character],
+    });
+  }
+}
+
+function fixAbnormalData() {
+  moveCharacters('House Lannister', 'Lannister');
+  moveCharacters('House Targaryen', 'Targaryan');
+  moveCharacters('House Baratheon', 'Baratheon');
+  moveCharacters('House Greyjoy', 'Greyjoy');
+  moveCharacters('House Tyrell', 'Tyrell');
+  moveCharacters('House Stark', 'Stark');
+
+  // moveCharacters('Lorathi', 'Lora');
+  //Unknowns
+  moveCharacters('Unknown', 'Unkown');
+  moveCharacters('Unknown', '');
+  moveCharacters('Unknown', 'None');
+}
+
+function moveCharacters(houseName, name) {
+  const index = house.findIndex((element) => element.name === name);
+  const houseIndex = house.findIndex((element) => element.name === houseName);
+
+  for (let i = 0; i < house[index].count; i++) {
+    house[houseIndex].count++;
+    house[houseIndex].characterArr.push(house[index].characterArr[i]);
+  }
+
+  //remove the element
+  house.splice(index, 1);
+}
+
+//Group characters that are alone
+function groupSingleCharacters() {
+  const baelish = house.findIndex(
+    (element) => element.name === 'House Baelish'
+  );
+
+  //Manually update a random alone characcter
+  house.push({
+    name: 'Alone',
+    count: 1,
+    characterArr: [house[baelish].characterArr[0]],
+  });
+  house.splice(baelish, 1);
+
+  house.forEach((element) => {
+    if (element.count === 1) {
+      moveCharacters('Alone', element.name);
+    }
+  });
+
+  //For some reason all the characters that have count === 1 are not moved :(
+  moveCharacters('Alone', 'House Seaworth');
+  moveCharacters('Alone', 'Naathi');
+  moveCharacters('Alone', 'Lorathi');
+  moveCharacters('Alone', 'Viper');
+  moveCharacters('Alone', 'Sand');
+  moveCharacters('Alone', 'Qyburn');
+}
 
 const renderChart = () => {
   const donutChart = document.querySelector('.donut-chart');
@@ -35,11 +127,11 @@ const renderChart = () => {
   new Chart(donutChart, {
     type: 'doughnut',
     data: {
-      labels: ['label', 'label', 'label', 'label'],
+      labels: house.map((houseElem) => houseElem.name),
       datasets: [
         {
           label: 'My First Dataset',
-          data: [1, 12, 33, 5],
+          data: house.map((houseElem) => houseElem.count),
           backgroundColor: backgroundColors,
           borderColor: borderColors,
           borderWidth: 1,
@@ -49,4 +141,4 @@ const renderChart = () => {
   });
 };
 
-renderChart();
+main();
